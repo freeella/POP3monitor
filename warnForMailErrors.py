f785687437ec9ccd9bc76eae6f2719969bd79e79
Kai Ellinger <coding@blicke.de>
2015-10-01 02:29:09 +0200

    added log to file

--- warnForMailErrors.py
+++ warnForMailErrors.py
@@ -19,7 +19,11 @@ import poplib       # accessing the POP3 account
 import logging      # for logging text
 
 # DEBUG
-is_debug = False
+is_debug   = False
+# TODO - can't redirect stack trace to log file
+#        only printing stack trace when using STDOUT
+#        need to fix later
+log_to_stdout = True
 
 # Printing stack trace
 def print_stack():
@@ -27,35 +31,65 @@ def print_stack():
 		'''
 		This function is used for debugging only.
 		'''
-		try:
-			raise TypeError("NeedStackTrace!")
-		except TypeError, err:
-			print "-" * 20, "BEGIN: STACKTRACE", "-" * 20
-			traceback.print_stack()
-			print "-" * 20, "END: STACKTRACE", "-" * 20
+		if log_to_stdout:
+			try:
+				raise TypeError("NeedStackTrace!")
+			except TypeError, err:
+				print "-" * 20, "BEGIN: STACKTRACE", "-" * 20
+				traceback.print_stack()
+				print "-" * 20, "END: STACKTRACE", "-" * 20
 
 # Read command line arguments
 def parse_arguments():
-	global is_debug
+	global is_debug, log_to_stdout
 	parser = argparse.ArgumentParser(description='Checks for hanging mails')
 	parser.add_argument('-v','--version', action='version', version='%(prog)s 1.0.1')
 	# if not calling python with -O option
+	# ORDER - having parameter INFO and DEBUG sets log level to DEBUG
+	parser.add_argument('-I','--INFO', action='store_const', dest='LOGLEVEL', const=logging.INFO, help='log level set to INFO')
+	# calling python with -O option disables debug information
 	if __debug__:
-		parser.add_argument('-D','--DEBUG', action='store_true', dest='DEBUG', help='log level set to DEBUG')
-	parser.add_argument('-I','--INFO', action='store_true', dest='INFO', help='log level set to INFO')
+		parser.add_argument('-D','--DEBUG', action='store_const', dest='LOGLEVEL', const=logging.DEBUG, help='log level set to DEBUG')
+	parser.add_argument('-L','--logfile', type=str, dest='LOGFILE', help='sets a file name to log to' )
 	parser.add_argument('-C','--config', type=str, dest='CONFIG', default="%s/%s" % ( os.environ['HOME'] , '.getmail/getmailrc' ), help='GETMAIL config file' )
 	args = parser.parse_args()
 
-	# setting log level as requested
+	# setting log format
 	# See: https://docs.python.org/2/library/logging.html#logrecord-attributes
-	logging_format_string = '%(asctime)s [%(levelname)s][%(pathname)s:%(lineno)4d][%(funcName)s] %(message)s'
-	if hasattr(args, 'DEBUG') and args.DEBUG:
+	logging_format_string = '%(asctime)s [%(levelname)-7s][%(pathname)s:%(lineno)4d][%(funcName)s] %(message)s'
+	# finish setting log level
+	if not hasattr(args, 'LOGLEVEL'):
+		args.LOGLEVEL = logging.WARNING
+	if args.LOGLEVEL == logging.DEBUG:
 		is_debug = True
-		logging.basicConfig(format=logging_format_string, level=logging.DEBUG)
-	elif hasattr(args, 'INFO') and args.INFO:
-		logging.basicConfig(format=logging_format_string, level=logging.INFO)
+	# log destination selection
+	write_to_log = False
+	log_file_error_msg = None
+	if hasattr(args, 'LOGFILE') and args.LOGFILE is not None:
+		# append existing log
+		if os.path.isfile( args.LOGFILE ):
+			# if existing file is writable; use it
+			write_to_log = os.access( args.LOGFILE, os.W_OK )
+			if not write_to_log:
+				log_file_error_msg = "LOG: File '%s' exists but is not writable! Using STDOUT!" % args.LOGFILE
+		# log file does not yet exist
+		else:
+			log_dir = os.path.dirname( args.LOGFILE )
+			# if args.LOGFILE does not contain a directory name
+			if '' == log_dir:
+				log_dir = '.'
+			# check dir permissions
+			write_to_log = os.access(log_dir, os.W_OK | os.X_OK)
+			if not write_to_log:
+				log_file_error_msg = "LOG: Directory '%s' is not writable! Using STDOUT!" % log_dir
+	# writing to STDOUT or to file?
+	if write_to_log:
+		logging.basicConfig(filename=args.LOGFILE, format=logging_format_string, level=args.LOGLEVEL)
+		log_to_stdout = False
 	else:
-		logging.basicConfig(format=logging_format_string, level=logging.WARNING)
+		logging.basicConfig(format=logging_format_string, level=args.LOGLEVEL)
+		if log_file_error_msg is not None:
+			logging.warn(log_file_error_msg)
 
 	# is the config file readable?
 	logging.info("CONFIG file: %s" % args.CONFIG )
